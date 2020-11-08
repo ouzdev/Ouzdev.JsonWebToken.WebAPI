007e82e37ececb4fc37280bbd5589834bcaf6e55
Oğuzcan Genç <mail@oguzcangenc.com>
2020-11-08 21:48:47 +0300

    Startup.cs içerisinde gerekli  JWT ayarları yapıldı. Token oluşturucu sınıfı kodlandı.

--- Ouzdev.JwtWebToken.WebAPI/Startup.cs
+++ Ouzdev.JwtWebToken.WebAPI/Startup.cs
@@ -1,9 +1,11 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Ouzdev.JwtWebToken.WebAPI
 {
@@ -22,10 +24,26 @@ namespace Ouzdev.JwtWebToken.WebAPI
             services.AddControllers();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
             {
+
+                //Https protokolunu iptal et.
                 opt.RequireHttpsMetadata = false;
+                // Tokenın özelliklerini belirleme. 
+                opt.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    //Tokenı kim oluşturdu ?
+                    ValidIssuer = "http://localhost",
+                    //Tokenı kim kullanacak ?
+                    ValidAudience = "http://localhost",
+                    //Token hangi key değerine göre çözülecek ?
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("ouzouzouz1")),
+                    //Token yukardaki keye göre çözülebilsinmi ?
+                    ValidateIssuerSigningKey = true,
 
+                    /*Bu token ne kadar süre yaşayacak ? Buradaki işlemi tokenı oluştururken tanımlayacağız. Yani token oluştururken ne kadar süre yaşayacağını belirteceğiz. */
+                    //Eğer tokenın süresi dolmuşsa sen o tokenı invalid yani geçersiz olarak kabul et. 
+                    ValidateLifetime = true,
 
-
+                };
 
             });
         }
---
+++ Ouzdev.JwtWebToken.WebAPI/TokenGenerator.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace Ouzdev.JwtWebToken.WebAPI
+{
+    public class TokenGenerator
+    {
+        public string CreateToken()
+        {
+            //Burada algoritma için bir anahtar (key) oluşturduk.
+            //**Buradaki anahtar startup.cs içinde yapılan tanımlamadaki anahtar değer ile eşleşirse token çalışacaktır(ufak bir not)
+            var bytes = Encoding.UTF8.GetBytes("ouzouzouz1");
+
+            //Sonrasında bu oluşturduğumuz keyi Simetrik anahtar generate eden nesnenin yapıcı metoduna tanımladık.
+            SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
+
+            //Simetrik anahtar nesnesini SignInCredentials nesnesinin yapıcı metoduna argüma olarak verdik.
+            //İkinci argüman olarak SecurityAlgorithms sınıfının HmacSha256 const değerini verdik. Jwt bu algoritmayı tavsiye eder.
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            //Issuer => Ureten (imzalayan)
+            //Audience => Tuketen
+            //notBefore => Hangi süreden önce geçerli değil ?. Şuan ki zamandan önce geçerli değil.
+            //expires => Ne zamana kadar geçerli ? şuanki tarihten 2 dakika sonrasına kadar geçerli.
+            //signinCredentials argümanına yukarıda oluştuduğum credentials nesnesini veriyorum.
+            JwtSecurityToken token = new JwtSecurityToken(issuer: "http://localhost", audience: "http://localhost", notBefore: DateTime.Now, expires: DateTime.Now.AddMinutes(2), signingCredentials: credentials);
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+
+            //Bu aşamada token oluşturulmuştur. Ve geriye JSON tipinde string bir değişken olarak return edilmiştir.
+            return handler.WriteToken(token);
+        }
+    }
+}
